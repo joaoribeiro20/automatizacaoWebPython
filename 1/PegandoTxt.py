82fd6b5155367960e301adaba80c08d4185a3034
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-06-16 16:58:00 -0300

    testes

--- 1/PegandoTxt.py
+++ 1/PegandoTxt.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get('https://www.amazon.com.br/')
-driver.find_element(By.XPATH, '//*[@id="nav-xshop"]/a[2]').click()
-driver.find_element(By.XPATH, '//*[@id="CardInstance07jsmzp3SWgoayOERej8-Q"]/div[2]/div[22]/a').click()
+driver.find_element(By.XPATH, '//*[@id="twotabsearchtextbox"]').send_keys("Pote Eletrolux")
+driver.implicitly_wait(0.5)
+driver.find_element(By.XPATH, '//*[@id="nav-search-submit-button"]').click()
+driver.implicitly_wait(6.5)
 
 
 
---
+++ 1/arquivo.py
@@ -0,0 +1,4 @@
+import csv
+
+# 1. abrir o arquivo
+with open("teste.csv", "r") as arquivo_referencia:
